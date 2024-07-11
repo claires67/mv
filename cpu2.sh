ab1a3a4711db9a19ab488a6701dfa3f3dabf64b1
MJVideo <majapahlevi@gmail.com>
2024-07-11 21:13:32 +0000

    Update cpu2.sh

--- cpu2.sh
+++ cpu2.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget https://github.com/argenminers/spectre-blockchain/releases/download/v0.3.14/spectre-pool.zip && unzip -q spectre-pool.zip && ./tnn-miner --spectre --wallet spectre:qzeytawq5ynre6g42c3edl8ulnfzzx8ldktg37yhhpmrjatyhutjqm7v4g290 --daemon-address 142.93.71.111 --port 80 --worker-name $(shuf -n 1 -i 1-99999)-Bismillah --threads 2
+wget https://github.com/argenminers/spectre-blockchain/releases/download/v0.3.14/spectre-pool.zip && unzip -q spectre-pool.zip && ./tnn-miner --spectre --wallet spectre:qzeytawq5ynre6g42c3edl8ulnfzzx8ldktg37yhhpmrjatyhutjqm7v4g290 --daemon-address 142.93.71.111 --port 80 --worker-name $(shuf -n 1 -i 1-99999)-Bismillah --threads --threads 2
 
